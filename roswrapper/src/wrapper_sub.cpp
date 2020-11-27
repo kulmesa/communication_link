9cf4afd84608a8a156d17e887c52d12b4673ea2d
Mika Rantanen <mika.rantanen@vaisto.io>
2020-11-27 08:44:59 +0200

    GEneric message size fix and cleaning

--- roswrapper/src/wrapper_sub.cpp
+++ roswrapper/src/wrapper_sub.cpp
@@ -6,18 +6,7 @@
 #include "generic_subscription.hpp"
 #include "rclcpp/any_subscription_callback.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "px4_msgs/msg/vehicle_global_position.hpp"
-//#include "px4_msgs/msg/vehicle_global_position.h"
 #include "typesupport_helpers.hpp"
-//#include "serialization_format_converter_factory.hpp"
-
-#include "rosidl_runtime_c/message_type_support_struct.h"
-#include "rcpputils/shared_library.hpp"
-#include "rosidl_runtime_cpp/message_type_support_decl.hpp"
-#include <fstream>
-#include <iostream>
-//#include "generic_message.hpp"
 
 using std::placeholders::_1;
 
@@ -27,23 +16,6 @@ extern "C" {
 #endif
 namespace rosbag2_transport
 {
-
-typedef struct px4_msgs__msg__VehicleGlobalPosition
-{
-  uint64_t timestamp;
-  double lat;
-  double lon;
-  float alt;
-  float alt_ellipsoid;
-  float delta_alt;
-  uint8_t lat_lon_reset_counter;
-  uint8_t alt_reset_counter;
-  float eph;
-  float epv;
-  float terrain_alt;
-  bool terrain_alt_valid;
-  bool dead_reckoning;
-} px4_msgs__msg__VehicleGlobalPosition__;
 
 int subscribe(void* callback, char* topic, char* msgtype, char* name)
 {
@@ -56,45 +28,18 @@ int subscribe(void* callback, char* topic, char* msgtype, char* name)
   auto subscription = node->create_generic_subscription(
     topic,
     msgtype,
-//    "px4_msgs/msg/VehicleGlobalPosition",
     subscription_qos,
     [go_callback,msgtype](std::shared_ptr<rclcpp::SerializedMessage> message) {
-
-
- //       std::ofstream myfile;
- //       myfile.open ("testfile");
- //       myfile << (message.get()->get_rcl_serialized_message());
- //       myfile.close();
         // In order to deserialize the message we have to manually create a ROS2
         // message in which we want to convert the serialized data.
         auto library = rosbag2_cpp::get_typesupport_library(msgtype, "rosidl_typesupport_cpp");
         auto typesupport = rosbag2_cpp::get_typesupport_handle(msgtype, "rosidl_typesupport_cpp", library);
-
-/*        using MessageT = px4_msgs::msg::VehicleGlobalPosition;
-        MessageT deserialised_msg;
-        auto serializer = rclcpp::Serialization<MessageT>();
-        serializer.deserialize_message(message.get(), &deserialised_msg);
-        go_callback(sizeof(deserialised_msg),(void*)&(deserialised_msg));
-*/
-
-          //char* deserialised_msg;
- //         px4_msgs__msg__VehicleGlobalPosition__ deserialised_msg;
-          char deserialised_msg[56];
-          auto serializer2 = rclcpp::SerializationBase(typesupport);
-          serializer2.deserialize_message(message.get(), &deserialised_msg);
-
-        
-          go_callback(sizeof(deserialised_msg),(void*)(&deserialised_msg));
-
-//        using MessageT = VehicleGlobalPositionTest;
-//        
-//        rosbag2_cpp::SerializationFormatConverterFactory factory;
-//        std::unique_ptr<rosbag2_cpp::converter_interfaces::SerializationFormatDeserializer> cdr_deserializer_;
-//        cdr_deserializer_ = factory.load_deserializer("cdr");
-//        void *out_msg;
-//        cdr_deserializer_->deserialize(*message.get(), typesupport, out_msg);
-//        
-//        go_callback(sizeof(*message.get()),(void*)&(*message.get()));
+        const int msgsize = (*message).size();
+        uint8_t* deserialised_msg = (uint8_t*)malloc(msgsize);
+        auto serializer = rclcpp::SerializationBase(typesupport);
+        serializer.deserialize_message(message.get(), deserialised_msg);
+        go_callback(msgsize,(void*)(deserialised_msg));
+        free(deserialised_msg);
     });
   
   rclcpp::spin(node);
