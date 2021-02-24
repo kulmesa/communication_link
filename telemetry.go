3323f3c20fcc06fd9794e067f3410c306df24c96
Mika Rantanen <mika.rantanen@vaisto.io>
2021-02-24 17:31:17 +0200

    New topics and subscribers added

--- telemetry.go
+++ telemetry.go
@@ -31,6 +31,24 @@ type sensorData struct {
 	MessageID  string
 }
 
+type batteryData struct {
+	BatteryStatus types.BatteryStatus
+	DeviceID      string
+	MessageID     string
+}
+
+type vehicleStatus struct {
+	VehicleStatus types.VehicleStatus
+	DeviceID      string
+	MessageID     string
+}
+
+type vehicleLocalPosition struct {
+	VehicleLocalPosition types.VehicleLocalPosition
+	DeviceID             string
+	MessageID            string
+}
+
 var rosStartTime uint64 = 0
 
 func sendGPSData(mqttClient mqtt.Client, gpsdata types.VehicleGlobalPosition) {
@@ -66,13 +84,52 @@ func sendSensorData(mqttClient mqtt.Client, data types.SensorCombined) {
 	mqttClient.Publish(topic, qos, retain, string(b))
 }
 
+func sendVehicleLocalPosition(mqttClient mqtt.Client, data types.VehicleLocalPosition) {
+	topic := fmt.Sprintf("/devices/%s/%s", *deviceID, "events/localposition")
+	u := uuid.New()
+	data.Timestamp = data.Timestamp + rosStartTime
+	t := vehicleLocalPosition{
+		VehicleLocalPosition: data,
+		DeviceID:             *deviceID,
+		MessageID:            u.String(),
+	}
+	b, _ := json.Marshal(t)
+	mqttClient.Publish(topic, qos, retain, string(b))
+}
+
+func sendVehicleStatus(mqttClient mqtt.Client, data types.VehicleStatus) {
+	topic := fmt.Sprintf("/devices/%s/%s", *deviceID, "events/vehiclestatus")
+	u := uuid.New()
+	data.Timestamp = data.Timestamp + rosStartTime
+	t := vehicleStatus{
+		VehicleStatus: data,
+		DeviceID:      *deviceID,
+		MessageID:     u.String(),
+	}
+	b, _ := json.Marshal(t)
+	mqttClient.Publish(topic, qos, retain, string(b))
+}
+
+func sendBatteryData(mqttClient mqtt.Client, data types.BatteryStatus) {
+	topic := fmt.Sprintf("/devices/%s/%s", *deviceID, "events/batterydata")
+	u := uuid.New()
+	data.Timestamp = data.Timestamp + rosStartTime
+	t := batteryData{
+		BatteryStatus: data,
+		DeviceID:      *deviceID,
+		MessageID:     u.String(),
+	}
+	b, _ := json.Marshal(t)
+	mqttClient.Publish(topic, qos, retain, string(b))
+}
+
 func handleGPSMessages(ctx context.Context, mqttClient mqtt.Client, node *ros.Node) {
 	messages := make(chan types.VehicleGlobalPosition)
 	log.Printf("Creating subscriber for %s", "VehicleGlobalPosition")
 	sub := node.InitSubscriber(messages, "VehicleGlobalPosition_PubSubTopic", "px4_msgs/msg/VehicleGlobalPosition")
 	go sub.DoSubscribe(ctx)
 	for m := range messages {
-		//			log.Printf("Lon: %f,  Lat:%f",m.Lat, m.Lon)
+		//		log.Printf("Lon: %f,  Lat:%f",m.Lat, m.Lon)
 		sendGPSData(mqttClient, m)
 	}
 	log.Printf("handleGPSMessages END")
@@ -92,8 +149,44 @@ func handleSensorMessages(ctx context.Context, mqttClient mqtt.Client, node *ros
 	sub.Finish()
 }
 
+func handleLocalPosMessages(ctx context.Context, mqttClient mqtt.Client, node *ros.Node) {
+	messages := make(chan types.VehicleLocalPosition)
+	log.Printf("Creating subscriber for %s", "VehicleLocalPosition")
+	sub := node.InitSubscriber(messages, "VehicleLocalPosition_PubSubTopic", "px4_msgs/msg/VehicleLocalPosition")
+	go sub.DoSubscribe(ctx)
+	for m := range messages {
+		sendVehicleLocalPosition(mqttClient, m)
+	}
+	log.Printf("handleSensorMessages END")
+	sub.Finish()
+}
+
+func handleStatusMessages(ctx context.Context, mqttClient mqtt.Client, node *ros.Node) {
+	messages := make(chan types.VehicleStatus)
+	log.Printf("Creating subscriber for %s", "SensorCombined")
+	sub := node.InitSubscriber(messages, "VehicleStatus_PubSubTopic", "px4_msgs/msg/VehicleStatus")
+	go sub.DoSubscribe(ctx)
+	for m := range messages {
+		sendVehicleStatus(mqttClient, m)
+	}
+	log.Printf("handleStatusMessages END")
+	sub.Finish()
+}
+
+func handleBatteryMessages(ctx context.Context, mqttClient mqtt.Client, node *ros.Node) {
+	messages := make(chan types.BatteryStatus)
+	log.Printf("Creating subscriber for %s", "BatteryStatus")
+	sub := node.InitSubscriber(messages, "Battery_PubSubTopic", "px4_msgs/msg/BatteryStatus")
+	go sub.DoSubscribe(ctx)
+	for m := range messages {
+		sendBatteryData(mqttClient, m)
+	}
+	log.Printf("handleBatteryMessages END")
+	sub.Finish()
+}
+
 func startTelemetry(ctx context.Context, wg *sync.WaitGroup, mqttClient mqtt.Client, node *ros.Node) {
-	wg.Add(2)
+	wg.Add(5)
 	go func() {
 		defer wg.Done()
 		handleGPSMessages(ctx, mqttClient, node)
@@ -101,5 +194,17 @@ func startTelemetry(ctx context.Context, wg *sync.WaitGroup, mqttClient mqtt.Cli
 	go func() {
 		defer wg.Done()
 		handleSensorMessages(ctx, mqttClient, node)
+	}()
+	go func() {
+		defer wg.Done()
+		handleLocalPosMessages(ctx, mqttClient, node)
+	}()
+	go func() {
+		defer wg.Done()
+		handleStatusMessages(ctx, mqttClient, node)
+	}()
+	go func() {
+		defer wg.Done()
+		handleBatteryMessages(ctx, mqttClient, node)
 	}()
 }
