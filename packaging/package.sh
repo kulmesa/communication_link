001d56c1f6e0b66e33d268b930b63d95ee9aa376
Jari Nippula <jarix@ssrc.tii.ae>
2021-03-17 15:37:13 +0200

    Fix CI build version

--- packaging/package.sh
+++ packaging/package.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-get_version() {
-	version=1.0."${BUILD_NUMBER}"~$(git describe --always --tags --dirty --match "[0-9]*.[0-9]*.[0-9]*")
-	echo "${version}"
+build_nbr=$1
+
+get_commit() {
+	echo $(git rev-parse HEAD)
 }
 
 build() {
@@ -19,6 +20,7 @@ build() {
 }
 
 make_deb() {
+	version=$1
 	echo "Creating deb package..."
 	build_dir=$(mktemp -d)
 	mkdir "${build_dir}/DEBIAN"
@@ -29,9 +31,20 @@ make_deb() {
 	cp communication_link "${build_dir}/usr/bin/"
 	cp videonode "${build_dir}/usr/bin/"
 
-	get_version
 	sed -i "s/VERSION/${version}/" "${build_dir}/DEBIAN/control"
 	cat "${build_dir}/DEBIAN/control"
+
+	# create changelog
+	pkg_name=$(grep -oP '(?<=Package: ).*' ${build_dir}/DEBIAN/control)
+	mkdir -p ${build_dir}/usr/share/doc/${pkg_name}
+	echo "${pkg_name} (${version}-0focal) focal; urgency=high" > ${build_dir}/usr/share/doc/${pkg_name}/changelog.Debian
+	echo >> ${build_dir}/usr/share/doc/${pkg_name}/changelog.Debian
+	echo "  * commit: $(get_commit)" >> ${build_dir}/usr/share/doc/${pkg_name}/changelog.Debian
+	echo >> ${build_dir}/usr/share/doc/${pkg_name}/changelog.Debian
+	echo " -- $(grep -oP '(?<=Maintainer: ).*' ${build_dir}/DEBIAN/control)  $(date +'%a, %d %b %Y %H:%M:%S %z')" >> ${build_dir}/usr/share/doc/${pkg_name}/changelog.Debian
+	echo >> ${build_dir}/usr/share/doc/${pkg_name}/changelog.Debian
+	gzip ${build_dir}/usr/share/doc/${pkg_name}/changelog.Debian
+
 	echo "communication-link_${version}_amd64.deb"
 	rm -rf ../communication-link_*.deb
 	fakeroot dpkg-deb --build "${build_dir}" "../communication-link_${version}_amd64.deb"
@@ -39,8 +52,6 @@ make_deb() {
 	echo "Done"
 }
 
-BUILD_NUMBER=${1:-0}
-
-version=$(get_version)
+version=1.0.${build_nbr}
 build
-make_deb
+make_deb $version
